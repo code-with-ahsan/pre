da6f4a1838a68be7f30d632a2e0d2f7dcce441c7
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-05-19 21:15:39 +0200

    end code for unit testing first video

--- react-testing/react-counter-app/src/App.test.tsx
+++ react-testing/react-counter-app/src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import App from './App';
 import { describe, it, expect } from 'vitest'
-import '@testing-library/jest-dom';
 
 describe('App component', () => {
   it('should render correct headline', () => {
--- react-testing/react-counter-app/src/App.tsx
+++ react-testing/react-counter-app/src/App.tsx
@@ -1,36 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    setCount((count) => count + 1)
-  }
-
-  const decrement = () => {
-    setCount((count) => count - 1)
-  }
-    
-  const reset = () => {
-    setCount(0)
-  }
   return (
     <>
       <h1 data-testid="headline" className='text-xl'>Counter React App</h1>
       <div className="card">
-        <h2 className='text-3xl mb-8'>
-          Count is {count}
-        </h2>
-        <div className="actions flex gap-4 items-center">
-          <button onClick={decrement}>
-            Decrement
-          </button><button onClick={reset}>
-            Reset
-          </button><button onClick={increment}>
-            Increment
-          </button>
-        </div>
+        <Counter />
       </div>
     </>
   )
