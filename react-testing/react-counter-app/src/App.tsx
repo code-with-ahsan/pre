e3b74751063349ec545f94e3aa0f8023d7e297bd
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-05-20 22:55:59 +0200

    unit tests async end code

--- react-testing/react-counter-app/src/App.tsx
+++ react-testing/react-counter-app/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Counter from './components/Counter'
+import CounterAsync from './components/CounterAsync'
+import CounterAsyncExternal from './components/CounterAsyncExternal';
 
 function App() {
   return (
     <>
       <h1 data-testid="headline" className='text-xl'>Counter React App</h1>
       <div className="card">
-        <Counter />
+        <CounterAsyncExternal />
       </div>
     </>
   )
