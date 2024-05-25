16e8b41b4d16d610a3b1c32a8ad7dcaff9aea976
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-05-25 16:37:04 +0200

    Revert "e2e-test final code"
    This reverts commit c5c8af3dac28ffd034ff7e141d15cddc676a9c5e.

--- react-testing/react-counter-app/src/lib/randomizer.ts
+++ react-testing/react-counter-app/src/lib/randomizer.ts
@@ -1,8 +1,5 @@
-export const MAX_NUM = 5;
+export const MAX_NUM = 5; 
 
 export const getRandomValue = () => {
-  if (import.meta.env.MODE === 'test') {
-    return '3';
-  }
   return Math.floor(Math.random() * MAX_NUM + 1).toString();
 }
